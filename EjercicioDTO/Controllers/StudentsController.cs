e63777f14e7ce05d261f59e6d3ae9c70844e898a
Dev <Dev@DESKTOP-1I2UJ50>
2021-02-21 22:45:03 -0600

    Add project files.

---
+++ EjercicioDTO/Controllers/StudentsController.cs
@@ -0,0 +1,169 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EjercicioDTO.Data;
+using EjercicioDTO.Data.Entities;
+using AutoMapper;
+using EjercicioDTO.DTO;
+
+namespace EjercicioDTO.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+        private readonly IMapper mapper;
+
+        public StudentsController(SchoolContext context, IMapper mapper)
+        {
+
+            _context = context;
+            this.mapper = mapper;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Students.ToListAsync();
+            var listStudents = data.Select(x => mapper.Map<StudentDTO>(x)).ToList();
+
+            return View(listStudents);
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var studentDTO = mapper.Map<StudentDTO>(student);
+            return View(studentDTO);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] StudentDTO studentDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = mapper.Map<Student>(studentDTO);
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(studentDTO);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var studentDTO = mapper.Map<StudentDTO>(student);
+            return View(studentDTO);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,EnrollmentDate")] StudentDTO studentDTO)
+        {
+            if (id != studentDTO.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        var student = mapper.Map<Student>(studentDTO);
+                        _context.Update(student);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    return View(studentDTO);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(studentDTO.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                
+            }
+            return View(studentDTO);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ EjercicioDTO/DTO/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EjercicioDTO.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjercicioDTO.DTO
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<StudentDTO, Student>();
+            CreateMap<Student, StudentDTO>();
+
+            
+        }
+    }
+}
+
